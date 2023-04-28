45fa8e4bcfb0b610ed93016c54d6fc4967f19721
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-04-28 08:14:32 +0300

    Add files via upload

--- data/users.py
+++ data/users.py
@@ -1,10 +1,13 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+
 from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
@@ -17,9 +20,14 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, unique=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    news = orm.relationship("News", back_populates='user')
+
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+
+
